21de309851969cb2716d71a349024b31001ad4db
Sheikh Mubashir <smubashir655@gmail.com>
2021-10-05 16:46:44 +0500

    d

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+// import React, { useEffect, useState } from 'react';
+// import axios from 'axios';
 
-function App() {
+// const App = () => {
+//   useEffect(() => {
+//     async function getData() {
+//       const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`);
+//       console.log(res.data.name)
+//       setName(res.data.name)
+//       setMoves(res.data.moves.length)
+//     }
+//     getData()
+//   })
+
+//   const [num, setNum] = useState();
+//   const [name, setName] = useState();
+//   const [moves, setMoves] = useState();
+//   return (
+//     <>
+//       <h1>we select {num} value </h1>
+//       <h1>my name is {name}</h1>
+//       <h1>i have {moves} moves </h1>
+
+//       <select value={num} onChange={(e) => { setNum(e.target.value) }}>
+//         <option value='1'>1</option>
+//         <option value='2'>2</option>
+//         <option value='3'>3</option>
+//         <option value='4'>4</option>
+//         <option value='5'>5</option>
+//       </select>
+
+//     </>
+//   )
+// }
+
+// export default App;
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import axios from 'axios';
+
+const App = () => {
+  const [num, setNum] = useState();
+  const [name, setName] = useState();
+  const [moves, setMoves] = useState();
+  useEffect(() => {
+    const getData = async () => {
+      const responce = await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`);
+      setName(responce.data.name);
+      setMoves(responce.data.moves.length)
+    }
+    getData()
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>The current value is <span style={{ color: 'yellow' }}>{num}</span></h1>
+      <h1>the name of the pokemin is <span style={{ color: 'red' }}>{name}</span></h1>
+      <h1>The {name} has <span style={{ color: 'blue' }}>{moves}</span> moves</h1>
+      <select value={num} onChange={(e) => { setNum(e.target.value) }}>
+        <option value='25'>25</option>
+        <option value='2'>2</option>
+        <option value='3'>3</option>
+        <option value='4'>4</option>
+        <option value='5'>5</option>
+      </select>
+    </>
+  )
 }
 
 export default App;
