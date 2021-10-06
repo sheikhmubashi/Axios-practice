c23faf63bf00a73dac5938005e62ad4a44967353
Sheikh Mubashir <smubashir655@gmail.com>
2021-10-06 11:31:05 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,63 +1,27 @@
-// import React, { useEffect, useState } from 'react';
-// import axios from 'axios';
-
-// const App = () => {
-//   useEffect(() => {
-//     async function getData() {
-//       const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`);
-//       console.log(res.data.name)
-//       setName(res.data.name)
-//       setMoves(res.data.moves.length)
-//     }
-//     getData()
-//   })
-
-//   const [num, setNum] = useState();
-//   const [name, setName] = useState();
-//   const [moves, setMoves] = useState();
-//   return (
-//     <>
-//       <h1>we select {num} value </h1>
-//       <h1>my name is {name}</h1>
-//       <h1>i have {moves} moves </h1>
-
-//       <select value={num} onChange={(e) => { setNum(e.target.value) }}>
-//         <option value='1'>1</option>
-//         <option value='2'>2</option>
-//         <option value='3'>3</option>
-//         <option value='4'>4</option>
-//         <option value='5'>5</option>
-//       </select>
-
-//     </>
-//   )
-// }
-
-// export default App;
-import React, { useEffect } from 'react';
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-
 const App = () => {
   const [num, setNum] = useState();
   const [name, setName] = useState();
   const [moves, setMoves] = useState();
+  const [change, setChange] = useState();
   useEffect(() => {
     const getData = async () => {
-      const responce = await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`);
-      setName(responce.data.name);
-      setMoves(responce.data.moves.length)
+      const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`)
+      setName(res.data.name);
+      setMoves(res.data.moves.length);
     }
     getData()
   })
-
   return (
     <>
-      <h1>The current value is <span style={{ color: 'yellow' }}>{num}</span></h1>
-      <h1>the name of the pokemin is <span style={{ color: 'red' }}>{name}</span></h1>
-      <h1>The {name} has <span style={{ color: 'blue' }}>{moves}</span> moves</h1>
+      <input value={change} onChange={(a) => { setChange(a.target.value) }}></input>
+      <h1>{change}</h1>
+      <h1>the current number is <span style={{ color: 'red' }}> {num}</span></h1>
+      <h1>the name of pokemon is <span style={{ color: 'blue' }}>{name}</span></h1>
+      <h1>{name} has <span style={{ color: 'green' }}>{moves}</span> moves</h1>
       <select value={num} onChange={(e) => { setNum(e.target.value) }}>
-        <option value='25'>25</option>
+        <option value='1'>1</option>
         <option value='2'>2</option>
         <option value='3'>3</option>
         <option value='4'>4</option>
